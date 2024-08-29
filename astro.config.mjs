e25f09a2a24652795adbd340bb56e239120f12d7
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-08-29 22:58:37 +0200

    remove unused shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), , react()]
+  integrations: [tailwind(), , react()]
 });
