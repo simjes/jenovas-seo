7945ae93fc9566a53def3f5ae02679efbfef4459
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-08-30 13:43:41 +0200

    init episode pages

---
+++ src/lib/meta.ts
@@ -0,0 +1,28 @@
+import type { Person } from "schema-dts";
+
+export const authors = [
+  {
+    "@type": "Person",
+    name: "Jostein Hakestad",
+    sameAs: "https://twitter.com/rad_crew",
+  },
+  {
+    "@type": "Person",
+    name: "Audun Rodem",
+    sameAs: "https://twitter.com/arodem",
+  },
+  {
+    "@type": "Person",
+    name: "Karl-Martin Hogsnes",
+    sameAs: "https://twitter.com/MuffensKarl",
+  },
+] satisfies Person[];
+
+
+export const slugify = (text: string) => text
+  .toLowerCase()
+  .replace(/[^\w\s-]/g, '') // Remove non-word chars (except spaces and hyphens)
+  .replace(/\s+/g, '-') // Replace spaces with hyphens
+  .replace(/--+/g, '-') // Replace multiple hyphens with single hyphen
+  .trim() /* Trim leading/trailing spaces and hyphens*/
+
--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -4,3 +4,16 @@ export const spotifyClient = SpotifyApi.withClientCredentials(
   import.meta.env.SPOTIFY_CLIENT_ID,
   import.meta.env.SPOTIFY_CLIENT_SECRET,
 );
+
+export const getShowDetails = async () => {
+  const showDetails = await spotifyClient.shows.get(
+    "0jxTtqiycCBwCcXGqGawoa",
+    "NO",
+  );
+  return showDetails;
+};
+
+export const getEpisodeDetails = async (id: string) => {
+  const episodeDetails = await spotifyClient.episodes.get(id, "NO");
+  return episodeDetails;
+};
