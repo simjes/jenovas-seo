ff0e3a9884c600fe06c8561e8ff2cb01a6c1b0f5
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-08-31 16:56:44 +0200

    canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jenovas-seo.vercel.app/', // TODO: to be changed
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
