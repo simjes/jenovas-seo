726ec165074ef383a2092998ebdddb317d80e637
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-09-01 20:33:18 +0200

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: 'https://www.jenovasvitner.no',
-  integrations: [react(), tailwind({
+  integrations: [sitemap(), react(), tailwind({
     applyBaseStyles: false,
   })],
   image: {
