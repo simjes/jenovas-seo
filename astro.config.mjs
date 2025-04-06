0f7e2928435cc6bacdb6adb7294816350127a908
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2025-04-06 12:54:44 +0200

    upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  site: 'https://www.jenovasvitner.no',
-  integrations: [sitemap(), react(), tailwind({
-    applyBaseStyles: false,
-  })],
+  site: "https://www.jenovasvitner.no",
+  integrations: [sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   image: {
     domains: ["i.scdn.co"],
-  }
+  },
 });
